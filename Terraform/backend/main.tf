21b202b90bf2d971d02255e432d67940b90fe4a6
Dmytro Kanarskyi <dmytro.ka@sbtech.com>
2025-05-15 04:13:51 +0300

    formatting

--- Terraform/backend/main.tf
+++ Terraform/backend/main.tf
@@ -31,3 +31,19 @@ resource "aws_s3_bucket_object_lock_configuration" "object_lock" {
     }
   }
 }
+
+resource "aws_dynamodb_table" "terraform_lock" {
+  name         = "terraform-state-lock"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+  
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+  
+  tags = {
+    Environment = "shared-services"
+    Purpose     = "terraform-state-locking"
+  }
+}
